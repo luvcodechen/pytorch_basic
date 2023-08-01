3698537809b5f3507092d65ebd71fa7523971108
luvcodechen <chen_ney@qq.com>
2023-08-01 09:59:06 +0800

    commit on 8.1

--- torchtest.py
+++ torchtest.py
@@ -29,6 +29,7 @@ print(device.type, device.index)
 print(device)
 print("------------------")
 data = np.array([1, 2, 3])
+print("data数据：", data)
 print(type(data))
 print(torch.Tensor(data))
 print(torch.tensor(data))
@@ -40,3 +41,26 @@ print(torch.zeros(2, 2))  # 零阵
 print(torch.ones(2, 2))  # 全1
 print(torch.rand(3, 3))  # 随机
 print("------------------")
+t1 = torch.Tensor(data)  # 构造类
+t2 = torch.tensor(data)  # 工厂函数
+t3 = torch.as_tensor(data)  # 工厂函数
+t4 = torch.from_numpy(data)  # 工厂函数
+print("torch_default_dtype: ", torch.get_default_dtype())
+print(t1, t1.dtype, '\n', t2, t2.dtype, '\n', t3, t3.dtype, '\n', t4, t4.dtype)
+print("------------------")
+print("修改原data数据后：")
+data[0] = data[1] = data[2] = 0
+print(t1, '\n', t2, '\n', t3, '\n', t4)
+print("------------------")
+t = torch.tensor([
+    [1, 1, 1, 1],
+    [2, 2, 2, 2],
+    [3, 3, 3, 3],
+], dtype=torch.float32)
+print(t.shape)
+print(t.size())
+print(len(t.shape))
+print(torch.tensor(t.shape).prod())
+print(t.numel())
+print("------------------")
+
