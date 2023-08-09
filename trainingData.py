c81d9f68bff11327d8a2c0aa68144c6cf3509b9a
luvcodechen <chen_ney@qq.com>
2023-08-09 10:37:40 +0800

    commit on 8.9

--- trainingData.py
+++ trainingData.py
@@ -1,3 +1,5 @@
+import os
+
 import pandas as pd
 import torch
 import torchvision
@@ -5,6 +7,10 @@ import torchvision.transforms as transforms
 from torch.utils import data
 import numpy as np
 import matplotlib.pyplot as plt
+import pylab
+import matplotlib
+
+matplotlib.use("Qt5Agg")  # 或者使用 "Qt5Agg"
 
 # class OHLC(Dataset):
 #     def __int__(self, csv_file):
@@ -23,11 +29,29 @@ import matplotlib.pyplot as plt
 train_set = torchvision.datasets.FashionMNIST(root='./data/FashionMNIST', train=True, download=True,
                                               transform=transforms.Compose([transforms.ToTensor()]))
 
-train_loader = torch.utils.data.DataLoader(train_set, batch_size=10)
+# train_loader = torch.utils.data.DataLoader(train_set, batch_size=10)
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=50)
 torch.set_printoptions(linewidth=120)
 print(len(train_set))
-print(train_set.train_labels)
-print(train_set.train_labels.bincount())
+print(train_set.targets)
+print(train_set.targets.bincount())
 sample = next(iter(train_set))
 print("len(sample): ", len(sample))
 print("type(sample): ", type(sample))
+image, label = sample
+print(image.shape)
+# print(label.shape)
+# plt.imshow(image.squeeze(), cmap="gray")
+# print("label:", label)
+# plt.show()
+batch = next(iter(train_loader))
+print(len(batch))
+print(type(batch))
+
+images, labels = batch
+print(images.shape)
+print(labels.shape)
+grid = torchvision.utils.make_grid(images, nrow=10)
+plt.figure(figsize=(15, 15), dpi=55)
+plt.imshow(np.transpose(grid, (1, 2, 0)))
+plt.show()
