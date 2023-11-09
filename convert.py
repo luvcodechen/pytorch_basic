a04e2338b1e712c8f23210b8126e2c01eb42f434
luvcodechen <chen_ney@qq.com>
2023-11-09 21:23:58 +0800

    CNN

---
+++ convert.py
@@ -0,0 +1,17 @@
+import json
+
+
+def convert_ipynb_to_py(ipynb_file, py_file):
+    with open(ipynb_file, 'r', encoding='utf-8') as f:
+        notebook = json.load(f)
+
+    with open(py_file, 'w', encoding='utf-8') as f:
+        for cell in notebook['cells']:
+            if cell['cell_type'] == 'code':
+                f.write(''.join(cell['source']) + '\n\n')
+
+
+    # convert_ipynb_to_py('Stack_vs_Caocat.ipynb', 'test.py')
+
+
+# convert_ipynb_to_py('cnntest.ipynb', 'test.py')
